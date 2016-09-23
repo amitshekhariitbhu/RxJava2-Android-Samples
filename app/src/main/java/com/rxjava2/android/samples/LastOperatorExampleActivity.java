ea24e56db6b21227aab1185f42a6f17b0c3bf8e7
AMIT SHEKHAR <amit.shekhar.iitbhu@gmail.com>
2016-09-23 14:18:40 +0530

    upgraded to RxJava2 RC3, few changes done in examples.

--- app/src/main/java/com/rxjava2/android/samples/LastOperatorExampleActivity.java
+++ app/src/main/java/com/rxjava2/android/samples/LastOperatorExampleActivity.java
@@ -7,9 +7,11 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
 import com.rxjava2.android.samples.utils.AppConstant;
+
 import io.reactivex.Observable;
-import io.reactivex.Observer;
+import io.reactivex.SingleObserver;
 import io.reactivex.disposables.Disposable;
 
 /**
@@ -17,57 +19,57 @@ import io.reactivex.disposables.Disposable;
  */
 public class LastOperatorExampleActivity extends AppCompatActivity {
 
-  private static final String TAG = DistinctExampleActivity.class.getSimpleName();
-  Button btn;
-  TextView textView;
+    private static final String TAG = DistinctExampleActivity.class.getSimpleName();
+    Button btn;
+    TextView textView;
 
 
-  @Override protected void onCreate(@Nullable Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_example);
-    btn = (Button) findViewById(R.id.btn);
-    textView = (TextView) findViewById(R.id.textView);
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_example);
+        btn = (Button) findViewById(R.id.btn);
+        textView = (TextView) findViewById(R.id.textView);
 
-    btn.setOnClickListener(new View.OnClickListener() {
-      @Override
-      public void onClick(View view) {
-        doSomeWork();
-      }
-    });
-  }
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                doSomeWork();
+            }
+        });
+    }
 
-  private void doSomeWork(){
-    getObservable().last() .subscribe(getObserver());
-  }
+    private void doSomeWork() {
+        getObservable().last("A1") // the default item ("A1") to emit if the source ObservableSource is empty
+                .subscribe(getObserver());
+    }
 
-  private Observable<String> getObservable() {
-    return Observable.just("A1", "A2", "A3", "A4", "A5", "A6");
-  }
+    private Observable<String> getObservable() {
+        return Observable.just("A1", "A2", "A3", "A4", "A5", "A6");
+    }
 
-  private Observer<String> getObserver() {
-    return new Observer<String>() {
+    private SingleObserver<String> getObserver() {
+        return new SingleObserver<String>() {
 
-      @Override
-      public void onSubscribe(Disposable d) {
-        Log.d(TAG, " onSubscribe : " + d.isDisposed());
-      }
+            @Override
+            public void onSubscribe(Disposable d) {
+                Log.d(TAG, " onSubscribe : " + d.isDisposed());
+            }
 
-      @Override public void onNext(String value) {
-        textView.append(" onNext : value : " + value);
-        textView.append(AppConstant.LINE_SEPARATOR);
-        Log.d(TAG, " onNext value : " + value);
-      }
+            @Override
+            public void onSuccess(String value) {
+                textView.append(" onNext : value : " + value);
+                textView.append(AppConstant.LINE_SEPARATOR);
+                Log.d(TAG, " onNext value : " + value);
+            }
 
 
-      @Override
-      public void onError(Throwable e) {
-        Log.d(TAG, " onError : " + e.getMessage());
-      }
-
-      @Override
-      public void onComplete() {
-        Log.d(TAG, " onComplete");
-      }
-    };
-  }
+            @Override
+            public void onError(Throwable e) {
+                textView.append(" onError : " + e.getMessage());
+                textView.append(AppConstant.LINE_SEPARATOR);
+                Log.d(TAG, " onError : " + e.getMessage());
+            }
+        };
+    }
 }
