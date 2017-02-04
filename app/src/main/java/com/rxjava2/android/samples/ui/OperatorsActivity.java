b29ee5ce4c44d441bfba03cfc2e6f7369b321973
AMIT SHEKHAR <amit.shekhar.iitbhu@gmail.com>
2017-02-04 11:42:15 +0530

    Add Networking Activity

--- app/src/main/java/com/rxjava2/android/samples/ui/OperatorsActivity.java
+++ app/src/main/java/com/rxjava2/android/samples/ui/OperatorsActivity.java
@@ -36,128 +36,128 @@ import com.rxjava2.android.samples.ui.operators.TimerExampleActivity;
 import com.rxjava2.android.samples.ui.operators.WindowExampleActivity;
 import com.rxjava2.android.samples.ui.operators.ZipExampleActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class OperatorsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_operators);
     }
 
     public void startSimpleActivity(View view) {
-        startActivity(new Intent(MainActivity.this, SimpleExampleActivity.class));
+        startActivity(new Intent(OperatorsActivity.this, SimpleExampleActivity.class));
     }
 
     public void startMapActivity(View view) {
-        startActivity(new Intent(MainActivity.this, MapExampleActivity.class));
+        startActivity(new Intent(OperatorsActivity.this, MapExampleActivity.class));
     }
 
     public void startZipActivity(View view) {
-        startActivity(new Intent(MainActivity.this, ZipExampleActivity.class));
+        startActivity(new Intent(OperatorsActivity.this, ZipExampleActivity.class));
     }
 
     public void startDisposableActivity(View view) {
-        startActivity(new Intent(MainActivity.this, DisposableExampleActivity.class));
+        startActivity(new Intent(OperatorsActivity.this, DisposableExampleActivity.class));
     }
 
     public void startTakeActivity(View view) {
-        startActivity(new Intent(MainActivity.this, TakeExampleActivity.class));
+        startActivity(new Intent(OperatorsActivity.this, TakeExampleActivity.class));
     }
 
     public void startTimerActivity(View view) {
-        startActivity(new Intent(MainActivity.this, TimerExampleActivity.class));
+        startActivity(new Intent(OperatorsActivity.this, TimerExampleActivity.class));
     }
 
     public void startIntervalActivity(View view) {
-        startActivity(new Intent(MainActivity.this, IntervalExampleActivity.class));
+        startActivity(new Intent(OperatorsActivity.this, IntervalExampleActivity.class));
     }
 
     public void startSingleObserverActivity(View view) {
-        startActivity(new Intent(MainActivity.this, SingleObserverExampleActivity.class));
+        startActivity(new Intent(OperatorsActivity.this, SingleObserverExampleActivity.class));
     }
 
     public void startCompletableObserverActivity(View view) {
-        startActivity(new Intent(MainActivity.this, CompletableObserverExampleActivity.class));
+        startActivity(new Intent(OperatorsActivity.this, CompletableObserverExampleActivity.class));
     }
 
     public void startFlowableActivity(View view) {
-        startActivity(new Intent(MainActivity.this, FlowableExampleActivity.class));
+        startActivity(new Intent(OperatorsActivity.this, FlowableExampleActivity.class));
     }
 
     public void startReduceActivity(View view) {
-        startActivity(new Intent(MainActivity.this, ReduceExampleActivity.class));
+        startActivity(new Intent(OperatorsActivity.this, ReduceExampleActivity.class));
     }
 
     public void startBufferActivity(View view) {
-        startActivity(new Intent(MainActivity.this, BufferExampleActivity.class));
+        startActivity(new Intent(OperatorsActivity.this, BufferExampleActivity.class));
     }
 
     public void startFilterActivity(View view) {
-        startActivity(new Intent(MainActivity.this, FilterExampleActivity.class));
+        startActivity(new Intent(OperatorsActivity.this, FilterExampleActivity.class));
     }
 
     public void startSkipActivity(View view) {
-        startActivity(new Intent(MainActivity.this, SkipExampleActivity.class));
+        startActivity(new Intent(OperatorsActivity.this, SkipExampleActivity.class));
     }
 
     public void startScanActivity(View view) {
-        startActivity(new Intent(MainActivity.this, ScanExampleActivity.class));
+        startActivity(new Intent(OperatorsActivity.this, ScanExampleActivity.class));
     }
 
     public void startReplayActivity(View view) {
-        startActivity(new Intent(MainActivity.this, ReplayExampleActivity.class));
+        startActivity(new Intent(OperatorsActivity.this, ReplayExampleActivity.class));
     }
 
     public void startConcatActivity(View view) {
-        startActivity(new Intent(MainActivity.this, ConcatExampleActivity.class));
+        startActivity(new Intent(OperatorsActivity.this, ConcatExampleActivity.class));
     }
 
     public void startMergeActivity(View view) {
-        startActivity(new Intent(MainActivity.this, MergeExampleActivity.class));
+        startActivity(new Intent(OperatorsActivity.this, MergeExampleActivity.class));
     }
 
     public void startDeferActivity(View view) {
-        startActivity(new Intent(MainActivity.this, DeferExampleActivity.class));
+        startActivity(new Intent(OperatorsActivity.this, DeferExampleActivity.class));
     }
 
     public void startDistinctActivity(View view) {
-        startActivity(new Intent(MainActivity.this, DistinctExampleActivity.class));
+        startActivity(new Intent(OperatorsActivity.this, DistinctExampleActivity.class));
     }
 
     public void startLastOperatorActivity(View view) {
-        startActivity(new Intent(MainActivity.this, LastOperatorExampleActivity.class));
+        startActivity(new Intent(OperatorsActivity.this, LastOperatorExampleActivity.class));
     }
 
     public void startReplaySubjectActivity(View view) {
-        startActivity(new Intent(MainActivity.this, ReplaySubjectExampleActivity.class));
+        startActivity(new Intent(OperatorsActivity.this, ReplaySubjectExampleActivity.class));
     }
 
     public void startPublishSubjectActivity(View view) {
-        startActivity(new Intent(MainActivity.this, PublishSubjectExampleActivity.class));
+        startActivity(new Intent(OperatorsActivity.this, PublishSubjectExampleActivity.class));
     }
 
     public void startBehaviorSubjectActivity(View view) {
-        startActivity(new Intent(MainActivity.this, BehaviorSubjectExampleActivity.class));
+        startActivity(new Intent(OperatorsActivity.this, BehaviorSubjectExampleActivity.class));
     }
 
     public void startAsyncSubjectActivity(View view) {
-        startActivity(new Intent(MainActivity.this, AsyncSubjectExampleActivity.class));
+        startActivity(new Intent(OperatorsActivity.this, AsyncSubjectExampleActivity.class));
     }
 
     public void startThrottleFirstActivity(View view) {
-        startActivity(new Intent(MainActivity.this,ThrottleFirstExampleActivity.class));
+        startActivity(new Intent(OperatorsActivity.this,ThrottleFirstExampleActivity.class));
     }
 
     public void startThrottleLastActivity(View view) {
-        startActivity(new Intent(MainActivity.this, ThrottleLastExampleActivity.class));
+        startActivity(new Intent(OperatorsActivity.this, ThrottleLastExampleActivity.class));
     }
 
     public void startDebounceActivity(View view) {
-        startActivity(new Intent(MainActivity.this, DebounceExampleActivity.class));
+        startActivity(new Intent(OperatorsActivity.this, DebounceExampleActivity.class));
     }
 
     public void startWindowActivity(View view) {
-        startActivity(new Intent(MainActivity.this,WindowExampleActivity.class));
+        startActivity(new Intent(OperatorsActivity.this,WindowExampleActivity.class));
     }
 
 
---
+++ app/src/main/java/com/rxjava2/android/samples/ui/networking/NetworkingActivity.java
@@ -0,0 +1,10 @@
+package com.rxjava2.android.samples.ui.networking;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by amitshekhar on 04/02/17.
+ */
+
+public class NetworkingActivity extends AppCompatActivity {
+}
